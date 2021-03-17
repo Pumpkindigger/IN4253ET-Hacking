d82c365809bf2f12e9abb9713f00e54d7f6cf07a
Suzanne Maquelin <suzanne@MacBook-Air-van-Suzanne.local>
2021-03-17 13:10:01 +0100

    first dockerfile setup

---
+++ IoTBox/setup.sh
@@ -0,0 +1,18 @@
+# Create bridge
+brctl addbr br0
+
+
+# Add interfaces to bridge
+brctl addif br0 eth0
+
+# Fix IPs
+ip addr flush dev eth0
+dhclient -v br0
+
+# Make sure everything is up
+ip link set eth0 up
+ip link set dev br0 up
+
+
+# Start QEMU instance
+qemu-system-x86_64 -drive format=raw,file=openwrt-19.07.7-x86-64-combined-ext4.img -nographic -net nic -net bridge,br=br0
