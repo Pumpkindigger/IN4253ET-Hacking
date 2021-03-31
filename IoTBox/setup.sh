443d935e5be8d9881f62d1100db7fe0d25cd25f2
Adriaan de Vos <adriaan.devos@gmail.com>
2021-03-31 17:32:27 +0200

    Add working ARM QEMU VM

--- IoTBox/setup.sh
+++ IoTBox/setup.sh
@@ -10,16 +10,6 @@ qemu-system-x86_64 -drive format=raw,file=/qemu_images/openwrt-19.07.7-x86-64-co
 # Start mips QEMU instance
 qemu-system-mips -kernel /qemu_images/openwrt-19.07.7-malta-be-vmlinux-initramfs.elf -net nic -net bridge,br=br0 -daemonize -serial telnet:localhost:4322,server,nowait;
 
-#Start arm qemu, does not yet work
-qemu-system-aarch64 -m 2G -M virt -cpu max \
-  -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd \
-  -drive if=none,file=/qemu_images/debian-10-openstack-arm64.qcow2,id=hd0 -device virtio-blk-device,drive=hd0 \
-  -net nic -net bridge,br=br0 \
-  -daemonize -serial telnet:localhost:4323,server,nowait;
+# Start ARM QEMU instance
+qemu-system-aarch64 -m 1024 -smp 2 -cpu cortex-a57 -M virt -kernel /qemu_images/openwrt-19.07.7-armvirt-64-Image-initramfs -net nic -net bridge,br=br0 -daemonize -serial telnet:localhost:4323,server,nowait;
 
-# Start PPC (does not work)
-# qemu-img create -f qcow2 /qemu_images/hda.img 3G
-# qemu-system-ppc -L pc-bios -boot d -M g3beige -m 1024 -cdrom qemu_images/debian-bullseye-DI-alpha3-ppc64el-netinst.iso -hda qemu_images/hda.img -g 1024x768x8 -nographic
-
-#Assign an IP address to the bridge, can probably be removed later as we no longer use br0
-#dhclient -v br0
